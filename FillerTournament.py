7d0f1bad77dc9613a6f53d3c54a208de26aeea2e
Christopher Ng <CNg@dev-10.com>
2022-04-08 11:54:28 -0500

    msg2

--- FillerTournament.py
+++ FillerTournament.py
@@ -13,6 +13,7 @@ name=''
 
 def startup():
     global validnumber
+    global numberpart
     print("Welcome to Tournaments R Us")
     while validnumber ==False:
         numberpart = int(input('Enter the number of participants: '))
@@ -21,21 +22,27 @@ def startup():
         else:
             print("Error try again")
     for slot in range(0,numberpart):
-        listpart.append('[Empty]')                 # for now
+        listpart.append('[Empty]')               
     print("There are ",numberpart," participant slots ready for sign-ups")    
 
 
 def signup():
+    global slotvalid
     print('''Participant Sign Up
             =====================''')
+    name = str(input('Participant Name: '))
+    print(name)
+    slotvalid=False
     while slotvalid==False:
-        name = str(input('Participant Name: '))
-        slotpref = int(input('Desired Starting slot #[1-',numberpart,']: '))                      #number part not assigned
-        if listpart[slotpref] == None:
-            listpart[slotpref] = name
+        slotpref = int(input(f'Desired Starting slot #[1-{numberpart}]: '))                      #number part not assigned
+        if listpart[slotpref-1] == '[Empty]':
+            listpart[slotpref-1] = name
+            #print(listpart)                     
             slotvalid=True
+            mainmenu()
         else:
             print("Slot is FULL try another slot")
+            mainmenu()
 
 def cancel():
     print('''Participant Cencellation
@@ -77,6 +84,29 @@ def exit():
         print("You didn't exit")
 
 
+def mainmenu():
+    print('''		
+        Participant Menu
+		================
+		1. Sign Up
+		2. Cancel Sign Up
+		3. View Participants
+		4. Save Changes
+		5. Exit''')
+
+    choice = int(input('What do you want to do (1-5): '))
+
+    if choice ==1:
+        signup()
+    if choice ==2:
+        cancel()
+    if choice ==3:
+        viewpart()
+    if choice ==4:
+        savechanges()
+    if choice ==5:
+        exit()
+
 
 while tour==False:
     starttour = str(input('Are you starting a tournament(Y/N): '))
@@ -86,28 +116,7 @@ while tour==False:
     else:
         tour = True
 
-#Main Menu
-print('''		
-        Participant Menu
-		================
-		1. Sign Up
-		2. Cancel Sign Up
-		3. View Participants
-		4. Save Changes
-		5. Exit''')
-
-choice = int(input('What do you want to do (1-5): '))
-
-if choice ==1:
-    signup()
-if choice ==2:
-    cancel()
-if choice ==3:
-    viewpart()
-if choice ==4:
-    savechanges()
-if choice ==5:
-    exit()
+mainmenu()
 
 
 
